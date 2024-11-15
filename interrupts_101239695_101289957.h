6ed3d73ab696e18054440422c674939e1062b850
Tchaoser <113814746+Tchaoser@users.noreply.github.com>
2024-11-15 11:08:15 -0500

    cool um
    structs moved to header for style

--- interrupts_101239695_101289957.h
+++ interrupts_101239695_101289957.h
@@ -7,8 +7,34 @@
 #include <stdlib.h>
 #include <time.h>
 
+struct partition {
+    unsigned int number;
+    unsigned int size;
+    int occupyingPID;
+};
+
+struct PCB {
+    unsigned int PID;
+    unsigned int CPU_Time;
+    unsigned int Arrival_Time;
+    unsigned int Mem_Size;
+    unsigned int IO_Freq;
+    unsigned int IO_Duration;
+    unsigned int Remaining_CPU;
+    unsigned int partitionInUse;
+    enum State {            // Current state of the process
+        NEW, READY, RUNNING, WAITING, TERMINATED
+    } state;
+};
+
+
 void routineScheduler();
 void memorySetup();
 void InputFileProcesser(FILE* traceFilePointer);
+void FcfsScheduler();
+void PriorityScheduler();
+void RoundRobinScheduler();
+void allocateMemory(PCB* pcb);
+
 
 #endif
